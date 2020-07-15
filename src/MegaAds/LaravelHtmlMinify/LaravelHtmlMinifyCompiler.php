74e4af72a8bda2646a592c6fd67a0dc5e7548c3c
Bach Nguyen <bachnx2303@gmail.com>
2020-07-15 10:16:55 +0700

    fix: Reverse origin code

--- src/MegaAds/LaravelHtmlMinify/LaravelHtmlMinifyCompiler.php
+++ src/MegaAds/LaravelHtmlMinify/LaravelHtmlMinifyCompiler.php
@@ -64,26 +64,20 @@ class LaravelHtmlMinifyCompiler extends BladeCompiler
     protected function compileMinify($value)
     {
         if ($this->shouldMinify($value)) {
-            $re = '%# Collapse whitespace everywhere but in blacklisted elements.
-                (?>             # Match all whitespans other than single space.
-                [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
-                | \s{2,}        # or two or more consecutive-any-whitespace.
-                ) # Note: The remaining regex consumes no text at all...
-                (?=             # Ensure we are not in a blacklist tag.
-                [^<]*+        # Either zero or more non-"<" {normal*}
-                (?:           # Begin {(special normal*)*} construct
-                    <           # or a < starting a non-blacklist tag.
-                    (?!/?(?:textarea|pre|script)\b)
-                    [^<]*+      # more non-"<" {normal*}
-                )*+           # Finish "unrolling-the-loop"
-                (?:           # Begin alternation group.
-                    <           # Either a blacklist start tag.
-                    (?>textarea|pre|script)\b
-                | \z          # or end of file.
-                )             # End alternation group.
-                )  # If we made it here, we are not in a blacklist tag.
-                %Six';
-            return preg_replace($re, '', $value);
+            $replace = array(
+                '/<!--[^\[](.*?)[^\]]-->/s' => '',
+                "/<\?php/"                  => '<?php ',
+                "/^\h*\/\/.*$/m"             => '',
+                "/\n([\S])/"                => ' $1',
+                "/\r/"                      => '',
+                "/\n/"                      => '',
+                "/\t/"                      => ' ',
+                "/ +/"                      => ' ',
+            );
+
+            return preg_replace(
+                array_keys($replace), array_values($replace), $value
+            );
         } else {
             return $value;
         }
